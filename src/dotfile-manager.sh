59dcfa57aafab43339bfc37572da0bf2a128a938
Jacob Koziej <jacobkoziej@gmail.com>
2020-07-15 22:20:55 -0400

    Define default storage directory

--- src/dotfile-manager.sh
+++ src/dotfile-manager.sh
@@ -14,3 +14,5 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+STORE_DIR="${DOTFILE_STORE_DIR:-$HOME/.dotfiles}"
