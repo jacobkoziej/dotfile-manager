5adfdecd9307a638e18c92f5cc6965aa68536bc7
Jacob Koziej <jacobkoziej@gmail.com>
2021-08-05 23:30:18 -0400

    Define ANSI SGR parameters

--- src/ansi-esc.h
+++ src/ansi-esc.h
@@ -20,4 +20,31 @@
 #define DOTFILE_MANAGER_ANSI_ESC
 
 
+#define ANSI_CSI "\033["
+
+
+/* Select Graphic Rendition */
+#define ANSI_SGR "m"
+
+#define ANSI_SGR_RESET     "0"
+#define ANSI_SGR_BOLD      "1"
+#define ANSI_SGR_LIGHT     "2"
+#define ANSI_SGR_ITALIC    "3"
+#define ANSI_SGR_UNDERLINE "4"
+
+#define ANSI_SGR_FOREGROUND        "3"
+#define ANSI_SGR_BACKGROUND        "4"
+#define ANSI_SGR_BRIGHT_FOREGROUND "9"
+#define ANSI_SGR_BRIGHT_BACKGROUND "10"
+
+#define ANSI_SGR_BLACK   "0"
+#define ANSI_SGR_RED     "1"
+#define ANSI_SGR_GREEN   "2"
+#define ANSI_SGR_YELLOW  "3"
+#define ANSI_SGR_BLUE    "4"
+#define ANSI_SGR_MAGENTA "5"
+#define ANSI_SGR_CYAN    "6"
+#define ANSI_SGR_WHITE   "7"
+
+
 #endif /* DOTFILE_MANAGER_ANSI_ESC */
